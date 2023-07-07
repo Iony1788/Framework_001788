7c416324deee4a4b3e5d9ae4de83daaced87e274
Iony1788 <ionyso685@gmail.com>
2023-07-07 19:47:09 +0200

    Sprint separation dossier

---
+++ TestFramework/WEB-INF/classes/Emp.java
@@ -0,0 +1,49 @@
+
+package etu1788.framework.servlet;
+import etu1788.framework.UrlMapping;;
+
+public class Emp {
+    int id;
+    String name;
+    String firstname;
+    
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    
+
+    @UrlMapping(url = "get-name.do")
+    public String getName() {
+        System.out.println(name);
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Emp(int id, String name, String firstname) {
+        this.id = id;
+        this.name = name;
+        this.firstname = firstname;
+    }
+
+    public Emp(String name) {
+        this.name = name;
+    }
+    
+    public Emp() {
+    }
+}
